793990dc84e61f389f7cd35ff29229bb2835a993
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-01-17 12:08:14 +0100

    2.4 step9 done

--- part2/2a_rendering_a_collection/src/Course.js
+++ part2/2a_rendering_a_collection/src/Course.js
@@ -1,4 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>;
+const Header = ({ name }) => <h2>{name}</h2>;
 
 const Total = ({ parts }) => {
   const sum = parts.reduce((acc, cur) => acc + cur.exercises, 0);
@@ -20,34 +20,11 @@ const Content = ({ parts }) => (
   </>
 );
 
-const Course = () => {
-  const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7,
-    },
-    {
-      name: 'State of a component',
-      exercises: 14,
-    },
-    {
-      name: 'redux',
-      exercises: 11,
-    },
-    {
-      name: 'random',
-      exercises: 100,
-    },
-  ];
-
+const Course = ({ course }) => {
+  const { name, parts } = course;
   return (
     <div>
-      <Header course={course} />
+      <Header name={name} />
       <Content parts={parts} />
       <Total
         // sum={parts[0].exercises + parts[1].exercises + parts[2].exercises}
