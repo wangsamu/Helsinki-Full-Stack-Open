37a0eb5f991d03c6bd13a7c12f1705a7c783a3c0
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-01-23 16:51:25 +0100

    2.11 phonebook step 6 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,28 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import AddContactForm from './AddContactForm';
 import ContactList from './ContactList';
 import SearchBar from './SearchBar';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
 
   const [searchValue, setSearchValue] = useState('');
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect!');
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('data fetched using useEffect!');
+      setPersons(response.data);
+    });
+  }, []);
 
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
