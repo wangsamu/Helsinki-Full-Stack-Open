3bfbe321975d6b89e21017e9543f155d57fa0762
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-02-01 18:22:33 +0100

    CountryData component added

---
+++ part2/countries/src/CountryData.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const CountryData = ({ country }) => {
+  return (
+    <div>
+      <h1>{country.name.common}</h1>
+      <p>Capital: {country.capital.map((city) => city)}</p>
+
+      <p>Area: {country.area} squared kms</p>
+      <p>Population: {country.population}</p>
+      <h3>Languages:</h3>
+      {Object.values(country.languages).map((language) => (
+        <p>{language}</p>
+      ))}
+      <img src={country.flags.png} />
+    </div>
+  );
+};
--- part2/countries/src/List.js
+++ part2/countries/src/List.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { CountryData } from './CountryData';
 
 export const List = ({ matchingCountries }) => {
   if (matchingCountries && matchingCountries.length > 10) {
     return <p>Too many results, please specify your search! 🤗</p>;
   }
+
+  if (matchingCountries && matchingCountries.length === 1) {
+    return <CountryData country={matchingCountries[0]} />;
+  }
+
   return (
     <div>
       {matchingCountries &&
