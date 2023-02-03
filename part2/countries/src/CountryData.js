b527c1f11d09d1f07cefe5272dec4ae95205ba1f
wangsamu <104201035+wangsamu@users.noreply.github.com>
2023-02-03 15:18:22 +0100

    Fetch weather data form openweatherapi and displaying icon, description, temperature and wind speed feature added

--- part2/countries/src/CountryData.js
+++ part2/countries/src/CountryData.js
@@ -2,19 +2,24 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 
 export const CountryData = ({ country }) => {
-  const [weatherData, setWeatherData] = useState([]);
+  const [weatherData, setWeatherData] = useState();
 
   const capitalLatitude = country.capitalInfo.latlng[0];
   const capitalLongitude = country.capitalInfo.latlng[1];
 
   console.log(capitalLatitude, capitalLongitude);
 
-  // useEffect(() => {
-  //   axios.get(
-  //     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon={lon}&appid={API key}`
-  //   );
-  //   setWeatherData();
-  // }, [country]);
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${capitalLatitude}&lon=${capitalLongitude}&appid=${process.env.REACT_APP_API_KEY}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setWeatherData(response.data);
+      });
+  }, [country]);
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -28,6 +33,18 @@ export const CountryData = ({ country }) => {
       ))}
       <img src={country.flags.png} />
       <h3>Weather in {country.capital[0]} now:</h3>
+      {weatherData && (
+        <div>
+          <img
+            src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+          />
+          <p>{weatherData.weather[0].description}</p>
+          <p>
+            temperature: {(weatherData.main.temp - 273.15).toFixed(2)} Celcius
+          </p>
+          <p>wind: {weatherData.wind.speed} m/s</p>
+        </div>
+      )}
     </div>
   );
 };
